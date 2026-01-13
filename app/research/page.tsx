70c39be7085bb944904507d2bcf1fef5bfe06be0
Ritesh Dhamale <ritesh.dhamale17@gmail.com>
2026-01-13 20:43:02 +0530

    Final edit

--- app/research/page.tsx
+++ app/research/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, Variants } from "framer-motion";
 
 export default function ResearchPage() {
   const containerVariant = {
@@ -11,7 +11,7 @@ export default function ResearchPage() {
     },
   };
 
-  const cardVariant = {
+  const cardVariant: Variants = {
     hidden: { opacity: 0, y: 40 },
     visible: {
       opacity: 1,
